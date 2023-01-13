2cdcc1c6acb4eb35341427ee408a2dd240908520
Shane O'Brien <guesswho@shane.pizza>
2023-01-13 19:12:24 +0000

    We added in movement and mouse rotation of the player, made camera a child of the player for free camera movement

--- DAV Unity/Assets/Scripts/FpsCharacterController.cs
+++ DAV Unity/Assets/Scripts/FpsCharacterController.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class FpsCharacterController : MonoBehaviour
 {
-
     [SerializeField] private float walkSpeed = 2.0f;
 
-    private float forward = 0.0f;
-    private float right = 0.0f;
+    private float verticalInput = 0.0f;
+    private float horizontalInput = 0.0f;
+    private float mouseInputX = 0.0f;
 
     private Rigidbody playerRb;
     
@@ -22,16 +22,33 @@ public class FpsCharacterController : MonoBehaviour
     private void Update()
     { 
         GetInputs();
+        GetMouseInputs();
+        MoveCharacter();
+        RotateCharacter();
     }
 
     private void GetInputs()
     {
-        forward = Input.GetAxis("Vertical");
-        right = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+    
+    private void GetMouseInputs()
+    {
+        mouseInputX = Input.GetAxis("Mouse X");
     }
 
     private void MoveCharacter()
     {
+        //playerRb.velocity = forward * new Vector3 (0, 0, 1) or new Vector3 (0, 0, -1)
+        playerRb.velocity = this.transform.forward * (verticalInput * walkSpeed);
         
+        //playerRb.velocity = right * new Vector3 (-1, 0, 1) or new Vector3 (1, 0, -1)
+        playerRb.velocity = playerRb.velocity + this.transform.right * (horizontalInput * walkSpeed);
+    }
+
+    private void RotateCharacter()
+    {
+        this.transform.Rotate(new Vector3(0.0f, mouseInputX, 0.0f));
     }
 }
